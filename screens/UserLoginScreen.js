cd24e380cfe0456c92a9954a1eb6753dbbc7ed10
BruceFergusonIII <bruce.ferguson3@gmail.com>
2020-02-19 23:51:33 -0600

    Layout for login screen has improved. Need to add the button to save user to storage.

--- screens/UserLoginScreen.js
+++ screens/UserLoginScreen.js
@@ -1,41 +1,93 @@
-import React, { useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Dimensions, StyleSheet, Text, View, TextInput, Keyboard, Image, Platform, Animated } from 'react-native';
 import { Button, Card, Header, Input, SearchBar, Icon} from "react-native-elements";
 
 const UserLoginScreen = (props) => {
 
-    const [fadeValue, setFadeValue] = useState(new Animated.Value(0));
+    const [slideUpValue] = useState(new Animated.Value(0));
+    const [loginFadeInValue] = useState(new Animated.Value(0));
+    const [fadeValue] = useState(new Animated.Value(0));
+    const [scaleValue] = useState(new Animated.Value(0));
 
     function _start() {
-        Animated.timing(fadeValue, {
-            toValue: 1,
-            duration: 1000
-        }).start();
+        return Animated.parallel([
+            Animated.timing(fadeValue, {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: true
+            }),
+            Animated.timing(slideUpValue, {
+                toValue: 1,
+                duration: 1000,
+                useNativeDriver: true
+            })
+        ]).start();
     }
 
-    function componentDidMount() {
-        _start()
+    function scaleImage() {
+        Animated.timing(scaleValue, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true
+        })
     }
+
+    function loginFadeIn() {
+        Animated.timing(loginFadeInValue, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true
+        }).start()
+    }
+
+    useEffect(() => {
+        _start();
+        setTimeout(() => loginFadeIn(), 1300)
+    }, []);
+
+
+
 
     return (
             <View style={styles.mainContainer}>
                 <View style={styles.logoContainer}>
-                    <Card containerStyle={{ marginTop: 60 }}>
-                        <Text style={styles.welcomeTitle}>Street Fighter V Player Tracker</Text>
-                    </Card>
+
                     <Image style={styles.fireSparks} source={require('../assets/images/source.gif')}/>
+
                     <Animated.View style={{
                         opacity: fadeValue,
+                        transform: [
+                            {
+                                translateY: slideUpValue.interpolate({
+                                    inputRange: [0, 1],
+                                    outputRange: [0, -55]
+                                })
+                            }
+                        ],
                     }}>
+                    <Card containerStyle={{ marginTop: 120 }}>
+                        <Text style={styles.welcomeTitle}>Street Fighter V Player Tracker</Text>
+                    </Card>
                     <Image style={styles.logo} source={require('../assets/images/street_fighter_v_fighting_logo-1001668.png')}/>
                     </Animated.View>
+
+                    <Animated.View style={{
+                        opacity: loginFadeInValue,
+                        transform: [
+                            {
+                                translateY: slideUpValue.interpolate({
+                                    inputRange: [0, 1],
+                                    outputRange: [0, -100]
+                                })
+                            }
+                        ],
+                    }}>
                     <View style={styles.signInContainer}>
-                        <Button type={'solid'} title={'Sign In'} style={styles.signInButton} onPress={props.checkIfUserExists}/>
                         <View>
                             <Input
                                 placeholder='Username'
                                 placeholderTextColor={'grey'}
-                                leftIcon={{ type: 'person', name: 'person', marginRight: 10}}
+                                leftIcon={{ type: 'person', name: 'person', marginRight: 10, color: 'grey'}}
                                 containerStyle={styles.userNameInput}
                                 style={styles.userName}
                                 onChange={props.recordUsername}
@@ -47,7 +99,7 @@ const UserLoginScreen = (props) => {
                                     <Icon
                                         name='lock'
                                         size={23}
-                                        color='black'
+                                        color='grey'
                                         containerStyle={{marginRight: 10}}
                                     />
                                 }
@@ -56,6 +108,7 @@ const UserLoginScreen = (props) => {
                             />
                         </View>
                     </View>
+                    </Animated.View>
                 </View>
             </View>
         )
@@ -94,22 +147,24 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-evenly',
-        backgroundColor: 'white',
+        backgroundColor: 'black',
         color: 'white',
-        height: Dimensions.get('window').height/7,
-        zIndex: 1000000
+        height: Dimensions.get('window').height/6,
+
     },
     signInButton: {
         width: Dimensions.get('window').width/3.5,
+        borderColor: 'white'
+
     },
     userNameInput: {
-        width: Dimensions.get('window').width/1.8,
+        width: Dimensions.get('window').width/1.5,
     },
     userName: {
         fontSize: 20
     },
     passwordInput: {
-        width: Dimensions.get('window').width/1.8,
+        width: Dimensions.get('window').width/1.5,
     },
     logo: {
         width: Dimensions.get('window').width,
@@ -118,7 +173,7 @@ const styles = StyleSheet.create({
         transform: [
             { translateY: Dimensions.get('window').height/100 },
         ],
-        zIndex: 10
+
     },
     ryu: {
         width: Dimensions.get('window').width,
@@ -150,7 +205,7 @@ const styles = StyleSheet.create({
         height: Dimensions.get('window').height,
         resizeMode: 'cover',
         position: 'absolute',
-        zIndex: 9
+
     },
     logoContainer: {
         width: '100%',
